cdc19cd124ee01c4a6e577900058b961c8660c30
jriosaguilar <joserios@kibostudios.com>
2020-05-29 18:35:09 +0200

    Añade header y footer mediante include

--- centro.php
+++ centro.php
@@ -1,114 +1,78 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>SPORT DESIGN - Actividades</title>
-        <meta charset="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <link rel="stylesheet" type="text/css" href="css/centro.css"/>
-        <link rel="stylesheet" type="text/css" href="bxslider-4-4.2.12/dist/jquery.bxslider.min.css"/>
-        <!-- Iconos diseñados por <a href="https://www.flaticon.es/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon"> www.flaticon.es</a> -->
-        <script type='text/javascript' src='jq/jquery-3.1.1.min.js'></script>
-        <script type="text/javascript" src="bxslider-4-4.2.12/src/js/jquery.bxslider.js"></script>
-        <script type="text/javascript" src="js/script.js"></script>
-        
-    </head>
-    <body>
-        <header class="sticky">
-            <nav>
-                <a href="index.html"><img src="Img/icono.svg" id="icono" alt="icono" title="icono"/></a>
-                <div>
-                    <h2>SPORT DESIGN</h2>
-                </div>
-                <label id="hamburguesa" class="container">
-                    <span class="bar1"></span>
-                    <span class="bar2"></span>
-                    <span class="bar3"></span>
-                </label>
-                <div id="myNav" class="overlay">
-                    <div class="overlay-content">
-                        <a href="index.php">Inicio</a>
-                        <a href="centro.php">Centro</a>
-                        <a href="actividades.php">Actividades</a>
-                        <a href="contacto.php">Contacto📞</a>
-                        <a href="login.php">Log In / Registro👤</a>
-                    </div>
-                </div>
-                <ul id="menu">
-                    <li><a href="index.php">Inicio</a></li>
-                    <li><a href="centro.php">Centro</a></li>
-                    <li><a href="actividades.php">Actividades</a></li>
-                    <li><a href="contacto.php">Contacto📞</a></li>
-                    <li><a href="login.php">Log In / Registro👤</a></li>
-                </ul>
-            </nav>
-            
-        </header>
 
-        <section>
-            <div id="info">
-                <h2>SPORT DESIGN</h2>
-                <div id="infoGeneral">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque quos, temporibus vel velit dolore cupiditate incidunt voluptatibus ratione distinctio suscipit odio provident quia aspernatur. Magni nulla aliquid laboriosam ratione exercitationem?</p>
-                    <img src="Img/instalaciones.jpg" alt="imagen cabecera" title="imagen cabecera"/>
-                </div>
-                <img src="Img/mapa.png" alt="mapa" title="mapa"/>
+<head>
+    <title>SPORT DESIGN - Actividades</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" type="text/css" href="css/centro.css" />
+    <link rel="stylesheet" type="text/css" href="bxslider-4-4.2.12/dist/jquery.bxslider.min.css" />
+    <!-- Iconos diseñados por <a href="https://www.flaticon.es/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon"> www.flaticon.es</a> -->
+    <script type='text/javascript' src='jq/jquery-3.1.1.min.js'></script>
+    <script type="text/javascript" src="bxslider-4-4.2.12/src/js/jquery.bxslider.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
+    <link rel="stylesheet" type="text/css" href="css/footer.css" />
+</head>
+
+<body>
+    <?php
+    include_once("header.php");
+    ?>
+    <section>
+        <div id="info">
+            <h2>SPORT DESIGN</h2>
+            <div id="infoGeneral">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque quos, temporibus vel velit dolore cupiditate incidunt voluptatibus ratione distinctio suscipit odio provident quia aspernatur. Magni nulla aliquid laboriosam ratione exercitationem?</p>
+                <img src="Img/instalaciones.jpg" alt="imagen cabecera" title="imagen cabecera" />
             </div>
+            <img src="Img/mapa.png" alt="mapa" title="mapa" />
+        </div>
 
-            <hr/>
+        <hr />
 
-            <div id="tarifas">
-                <h2>Tarifas</h2>
-                <div class="opcion">
-                    <img src="Img/instalaciones.jpg" alt="instalaciones" title="instalaciones"/>
-                    <div class="textoOpcion">
-                        <h2>Plan de pago *</h2>
-                        <br/>
-                        <h2>* €</h2>
-                    </div>
-                </div>
-                <div class="opcion">
-                    <img src="Img/actividades.jpg" alt="actividades" title="actividades"/>
-                    <div class="textoOpcion">
-                        <h2>Plan de pago *</h2>
-                        <br/>
-                        <h2>* €</h2>
-                    </div>
+        <div id="tarifas">
+            <h2>Tarifas</h2>
+            <div class="opcion">
+                <img src="Img/instalaciones.jpg" alt="instalaciones" title="instalaciones" />
+                <div class="textoOpcion">
+                    <h2>Plan de pago *</h2>
+                    <br />
+                    <h2>* €</h2>
                 </div>
             </div>
-
-            <hr/>
-
-            <div id="instalaciones">
-                <h2>Instalaciones</h2>
-                <div class="fotoInstalaciones">
-                    <img src="Img/instalaciones.jpg" alt="instalaciones" title="instalaciones"/>
-                </div>
-                <div class="fotoInstalaciones">
-                    <img src="Img/actividades.jpg" alt="actividades" title="actividades"/>
-                </div>
-                <div class="fotoInstalaciones">
-                    <img src="Img/registrate.jpeg" alt="actividades" title="actividades"/>
-                </div>
-                <div class="fotoInstalaciones">
-                    <img src="Img/fondo.jpg" alt="actividades" title="actividades"/>
+            <div class="opcion">
+                <img src="Img/actividades.jpg" alt="actividades" title="actividades" />
+                <div class="textoOpcion">
+                    <h2>Plan de pago *</h2>
+                    <br />
+                    <h2>* €</h2>
                 </div>
             </div>
+        </div>
 
-        </section>
+        <hr />
 
-        <footer>
-            <img src="Img/icono.svg" id="iconoFooter" alt="icono" title="icono"/>
-            <h3>SPORT DESIGN</h3>
-            <a href="contacto.php" id="linkContacto">Contacto</a>
-            <a href="#" id="avisoLegal">Aviso legal</a>
-            <ul id="contacto">
-                <li><p>WhatsApp: 654987654</p></li>
-                <li><p>Correo: NaturAir@gmail.com</p></li>
-            </ul>
-            <ul id="redesSociales">
-                <li><a href="#"><img src="Img/twitter.png" alt="twitter" title="twitter"/></a></li>
-                <li><a href="#"><img src="Img/facebook.png" alt="facebook" title="facebook"/></a></li>
-            </ul>
-        </footer>
-    </body>
+        <div id="instalaciones">
+            <h2>Instalaciones</h2>
+            <div class="fotoInstalaciones">
+                <img src="Img/instalaciones.jpg" alt="instalaciones" title="instalaciones" />
+            </div>
+            <div class="fotoInstalaciones">
+                <img src="Img/actividades.jpg" alt="actividades" title="actividades" />
+            </div>
+            <div class="fotoInstalaciones">
+                <img src="Img/registrate.jpeg" alt="actividades" title="actividades" />
+            </div>
+            <div class="fotoInstalaciones">
+                <img src="Img/fondo.jpg" alt="actividades" title="actividades" />
+            </div>
+        </div>
+
+    </section>
+    <?php
+    include_once("footer.php");
+    ?>
+</body>
+
 </html>
