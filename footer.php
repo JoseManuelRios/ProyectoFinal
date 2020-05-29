ad9fca4275607204b57441e55f0d197b34254374
jriosaguilar <joserios@kibostudios.com>
2020-05-29 18:33:51 +0200

    Separa en un archivo distinto el footer de las páginas

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer>
+    <img src="Img/icono.svg" id="iconoFooter" alt="icono" title="icono" />
+    <h3>SPORT DESIGN</h3>
+    <a href="contacto.php" id="linkContacto">Contacto</a>
+    <a href="#" id="avisoLegal">Aviso legal</a>
+    <ul id="contacto">
+        <li>
+            <p>WhatsApp: 654987654</p>
+        </li>
+        <li>
+            <p>Correo: NaturAir@gmail.com</p>
+        </li>
+    </ul>
+    <ul id="redesSociales">
+        <li><a href="#"><img src="Img/twitter.png" alt="twitter" title="twitter" /></a></li>
+        <li><a href="#"><img src="Img/facebook.png" alt="facebook" title="facebook" /></a></li>
+    </ul>
+</footer>
