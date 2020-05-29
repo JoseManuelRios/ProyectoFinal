aa3ce9a9362876a64518a9e48ca8ee9610394fe8
jriosaguilar <joserios@kibostudios.com>
2020-05-29 18:32:20 +0200

    Añade la página para registrar un usuario

---
+++ registro.php
@@ -0,0 +1,87 @@
+<?php
+session_name("gimnasio");
+session_start();
+
+include("consumir_servicio.php");
+
+$error_apellidos=true;
+$error_clave=true;
+$error_nombre=true;
+$error_telefono=true;
+$error_edad=true;
+$error_correo=true;
+$error_direccion=true;
+$error_ciudad=true;
+$error_tarjeta=true;
+
+if(isset($_POST["btnRegistro"])){
+    $error_nombre=$_POST["nombre"]=="";
+    $error_apellidos=$_POST["apellidos"]=="";
+    $error_clave=$_POST["clave"]=="";
+    $error_telefono=$_POST["telefono"]=="" || !is_numeric($_POST["telefono"]);
+    $error_edad=$_POST["edad"]=="" || !is_numeric($_POST["edad"]);
+    $error_correo=$_POST["correo"]=="" || !filter_var($_POST["correo"],FILTER_VALIDATE_EMAIL);
+    $error_direccion=$_POST["direccion"]=="";
+    $error_ciudad=$_POST["ciudad"]=="";
+    $error_tarjeta=$_POST["tarjeta"]==""  || !is_numeric($_POST["tarjeta"]);
+
+    $correcto=!$error_nombre && !$error_apellidos && !$error_clave && !$error_telefono && !$error_edad && !$error_correo && !$error_direccion && !$error_ciudad && !$error_tarjeta;
+
+    if($correcto){
+        $datosInsertar=Array("nombre"=>$_POST["nombre"],"apellidos"=>$_POST["apellidos"],"clave"=>$_POST["clave"],"telefono"=>$_POST["telefono"],"edad"=>$_POST["edad"],"correo"=>$_POST["correo"],"direccion"=>$_POST["direccion"],"ciudad"=>$_POST["ciudad"],"observaciones"=>$_POST["observaciones"],"tarjeta"=>$_POST["tarjeta"]);
+        $obj=consumir_servicio_REST($enlace."/registroUsuario","POST",$datosInsertar);
+
+        if(isset($obj->mensaje_error)){
+            die($obj->mensaje_error);
+        }else{
+            echo $obj->mensaje;
+            $_SESSION["nombre"]=$_POST["usuario"];
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>SPORT DESIGN - Log In</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" type="text/css" href="css/registro.css" />
+    <!-- Iconos diseñados por <a href="https://www.flaticon.es/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon"> www.flaticon.es</a> -->
+    <script type='text/javascript' src='jq/jquery-3.1.1.min.js'></script>
+    <script type="text/javascript" src="js/script.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
+    <link rel="stylesheet" type="text/css" href="css/footer.css" />
+</head>
+
+<body>
+    <?php
+    include_once("header.php");
+    ?>
+    <section>
+        <div class="contenedor">
+            <div class="contenedor2">
+                <h2>Registrarse</h2>
+                <form method="post" action="registro.php">
+                    <label for="nombre">Nombre</label><input type="text" id="nombre" name="nombre" value="" />
+                    <label for="apellidos">Apellidos</label><input type="text" id="apellidos" name="apellidos" />
+                    <label for="clave">Clave</label><input type="password" id="clave" name="clave" />
+                    <label for="telefono">Telefono</label><input type="text" id="telefono" name="telefono" />
+                    <label for="edad">Edad</label><input type="number" id="edad" name="edad" />
+                    <label for="correo">Correo</label><input type="text" id="correo" name="correo" />
+                    <label for="direccion">Direccion</label><input type="text" id="direccion" name="direccion" />
+                    <label for="ciudad">Ciudad</label><input type="text" id="ciudad" name="ciudad" />
+                    <label for="tarjeta">Nº Tarjeta: </label><input type="text" id="tarjeta" name="tarjeta" />
+                    <label for="observaciones">Observaciones</label><textarea id="observaciones" name="observaciones" rows="7" cols="40"></textarea>
+                    <button type="submit" name="btnRegistro">Registrarse</button>
+                </form>
+            </div>
+        </div>
+    </section>
+    <?php
+    include_once("footer.php");
+    ?>
+</body>
+
+</html>
