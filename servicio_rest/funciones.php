5dae80d8c25c78b443bb109553fc8313fe4a11d5
jriosaguilar <joserios@kibostudios.com>
2020-05-29 19:18:35 +0200

    Crea la función la cual va a obtener todos los planes de pago.

--- servicio_rest/funciones.php
+++ servicio_rest/funciones.php
@@ -1,17 +1,44 @@
 <?php
     require "conexion_bd.php";
 
-    function registrarUsuario($nombre,$apellidos,$clave,$telefono,$edad,$correo,$direccion,$ciudad,$observaciones,$tarjeta){
+    function registrarUsuario($planPago,$nombre,$apellidos,$clave,$telefono,$edad,$correo,$direccion,$ciudad,$observaciones,$tarjeta){
         $con=conectar();
         if(!$con){
             return array("mensaje_error"=>"Imposible conectar. Error número ".mysqli_connect_errno().":".mysqli_connect_error());
         }else{
             mysqli_set_charset($con,"utf8");
-            $consulta="INSERT into clientes(nombre,apellidos,clave,telefono,edad,correo,direccion,ciudad,observaciones,tarjeta) values('".$nombre."','".$apellidos."','".$clave."','".$telefono."','".$edad."','".$correo."','".$direccion."','".$ciudad."','".$observaciones."','".$tarjeta."')";
+            $consulta="INSERT into clientes(idPlanPago,nombre,apellidos,clave,telefono,edad,correo,direccion,ciudad,observaciones,tarjeta) values('".$planPago."','".$nombre."','".$apellidos."','".$clave."','".$telefono."','".$edad."','".$correo."','".$direccion."','".$ciudad."','".$observaciones."','".$tarjeta."')";
 
             if($resultado=mysqli_query($con,$consulta)){
                 $mensaje="Usuario insertado con éxito";
             }else{ 
+                $mensaje="Imposible conectar. Error número ".mysqli_errno($con).":".mysqli_error($con);
+                mysqli_close($con);
+                return array("mensaje_error"=>$mensaje);
+            }
+        }
+    }
+
+    function obtenerPlanesPago(){
+        $con=conectar();
+        if(!$con){
+            return array("mensaje_error"=>"Imposible conectar. Error número ".mysqli_connect_errno().":".mysqli_connect_error());
+        }else{
+            mysqli_set_charset($con,"utf8");
+            $consulta="SELECT * FROM planpago";
+            
+            if($resultado=mysqli_query($con,$consulta)){
+
+                $planesPago=Array();
+                while($fila=mysqli_fetch_assoc($resultado)){
+                    $planesPago[]=$fila;
+                }
+
+                mysqli_free_result($resultado);
+                mysqli_close($con);
+                return array("planesPago"=>$planesPago);
+
+            }else{
                 $mensaje="Imposible conectar. Error número ".mysqli_errno($con).":".mysqli_error($con);
                 mysqli_close($con);
                 return array("mensaje_error"=>$mensaje);
