c406b151dc052daefe0ccbe79a5d8cacb51393e7
jriosaguilar <joserios@kibostudios.com>
2020-05-30 18:56:44 +0200

    Crea la función de login

--- servicio_rest/funciones.php
+++ servicio_rest/funciones.php
@@ -45,6 +45,37 @@
             }
         }
     }
+
+    function login($id,$clave){
+        $con=conectar();
+        if(!$con){
+            return array("mensaje_error"=>"Imposible conectar. Error número ".mysqli_connect_errno().":".mysqli_connect_error());
+        }else{
+            mysqli_set_charset($con,"utf8");
+            $consulta="SELECT * FROM clientes WHERE idCliente='".$id."' and clave='".$clave."'";
+            
+            if($resultado=mysqli_query($con,$consulta)){
+
+                if(mysqli_num_rows($resultado)>0){
+                    $cliente=$resultado;
+                }else{
+                    $mensaje="Usuario no registrado. Introduce unos datos correctos";
+                    mysqli_close($con);
+                    return array("mensaje"=>$mensaje);
+                }
+
+                mysqli_free_result($resultado);
+                mysqli_close($con);
+                return array("cliente"=>$cliente);
+
+            }else{
+                $mensaje="Imposible conectar. Error número ".mysqli_errno($con).":".mysqli_error($con);
+                mysqli_close($con);
+                return array("mensaje_error"=>$mensaje);
+            }
+        }
+    }
+
     /*function obtener_productos(){
         $con=conectar();
         if(!$con){
