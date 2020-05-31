992a37b1909c25cb190cbe1eee9e8e7e8bdd6516
jriosaguilar <joserios@kibostudios.com>
2020-05-31 19:07:08 +0200

    Añade pagina admin, donde se administrará toda la BD

---
+++ admin.php
@@ -0,0 +1,37 @@
+<?php
+session_name("gimnasio");
+session_start();
+
+include("consumir_servicio.php");
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>SPORT DESIGN</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" type="text/css" href="css/" />
+    <!-- Iconos diseñados por <a href="https://www.flaticon.es/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon"> www.flaticon.es</a> -->
+    <script type='text/javascript' src='jq/jquery-3.1.1.min.js'></script>
+    <script type="text/javascript" src="js/script.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
+    <link rel="stylesheet" type="text/css" href="css/footer.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <?php
+    include_once("header.php");
+    ?>
+
+    <section>
+        
+    </section>
+
+    <?php
+    include_once("footer.php");
+    ?>
+</body>
+</html>
