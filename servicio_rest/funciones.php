0c2386b3ced42d338e419c4996f290d62fa1a036
jriosaguilar <joserios@kibostudios.com>
2020-06-07 19:10:34 +0200

    Añade la funcion para añadir una actividad

--- servicio_rest/funciones.php
+++ servicio_rest/funciones.php
@@ -150,6 +150,26 @@
         }
     }
 
+    function aniadirActividad($nombre,$descripcion,$maximo,$aforo,$formacion){
+        $con=conectar();
+        if(!$con){
+            return array("mensaje_error"=>"Imposible conectar. Error número ".mysqli_connect_errno().":".mysqli_connect_error());
+        }else{
+            mysqli_set_charset($con,"utf8");
+            $consulta="INSERT into jmra_actividades(nombre,descripcion,maximo,aforo,formacion) values('".$nombre."','".$descripcion."','".$maximo."','".$aforo."','".$formacion."')";
+
+            if($resultado=mysqli_query($con,$consulta)){
+                $id=mysqli_insert_id($con);
+                $mensaje="Actividad introducida correctamente";
+                return array("mensaje"=>$mensaje);
+            }else{ 
+                $mensaje="Imposible conectar. Error número ".mysqli_errno($con).":".mysqli_error($con);
+                mysqli_close($con);
+                return array("mensaje_error"=>$mensaje);
+            }
+        }
+    }
+
     /*function obtener_productos(){
         $con=conectar();
         if(!$con){
