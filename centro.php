02dd385a9b0645008c83abb12e435c835833e7c4
jriosaguilar <joserios@kibostudios.com>
2020-06-09 18:59:25 +0200

    Añade el div slider donde se pasaran las imagenes del centro

--- centro.php
+++ centro.php
@@ -11,11 +11,11 @@ include("consumir_servicio.php");
     <title>SPORT DESIGN - Actividades</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <link rel="stylesheet" type="text/css" href="css/centro.css" />
-    <link rel="stylesheet" type="text/css" href="bxslider-4-4.2.12/dist/jquery.bxslider.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/jquery.bxslider.min.css"/>
+    <link rel="stylesheet" type="text/css" href="css/centro.css"/>
     <!-- Iconos diseñados por <a href="https://www.flaticon.es/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon"> www.flaticon.es</a> -->
     <script type='text/javascript' src='jq/jquery-3.1.1.min.js'></script>
-    <script type="text/javascript" src="bxslider-4-4.2.12/src/js/jquery.bxslider.js"></script>
+    <script type='text/javascript' src='js/jquery.bxslider.js'></script>
     <script type="text/javascript" src="js/script.js"></script>
     <link rel="stylesheet" type="text/css" href="css/header.css" />
     <link rel="stylesheet" type="text/css" href="css/footer.css" />
@@ -48,7 +48,7 @@ include("consumir_servicio.php");
                 }else{
                     foreach($obj->tabla as $fila){
                         echo "<div class='opcion'>";
-                            echo "<img src='Img/instalaciones.jpg' alt='instalaciones' title='instalaciones' />";
+                            echo "<img src='Img/".$fila->foto."' alt='instalaciones' title='instalaciones' />";
                             echo "<div class='textoOpcion'>";
                                 echo "<h2>Plan de pago - ".$fila->nombre."</h2>";
                                 echo "<br/>";
@@ -58,39 +58,24 @@ include("consumir_servicio.php");
                     }
                 }
             ?>
-            <!--<div class="opcion">
-                <img src="Img/instalaciones.jpg" alt="instalaciones" title="instalaciones" />
-                <div class="textoOpcion">
-                    <h2>Plan de pago *</h2>
-                    <br />
-                    <h2>* €</h2>
-                </div>
-            </div>
-            <div class="opcion">
-                <img src="Img/actividades.jpg" alt="actividades" title="actividades" />
-                <div class="textoOpcion">
-                    <h2>Plan de pago *</h2>
-                    <br />
-                    <h2>* €</h2>
-                </div>
-            </div>
-        </div>-->
 
         <hr />
 
         <div id="instalaciones">
             <h2>Instalaciones</h2>
-            <div class="fotoInstalaciones">
-                <img src="Img/instalaciones.jpg" alt="instalaciones" title="instalaciones" />
-            </div>
-            <div class="fotoInstalaciones">
-                <img src="Img/actividades.jpg" alt="actividades" title="actividades" />
-            </div>
-            <div class="fotoInstalaciones">
-                <img src="Img/registrate.jpeg" alt="actividades" title="actividades" />
-            </div>
-            <div class="fotoInstalaciones">
-                <img src="Img/fondo.jpg" alt="actividades" title="actividades" />
+            <div class="slider">
+                <div>
+                    <img src="Img/instalaciones.jpg" alt="instalaciones" title="instalaciones" />
+                </div>
+                <div>
+                    <img src="Img/actividades.jpg" alt="actividades" title="actividades" />
+                </div>
+                <div>
+                    <img src="Img/registrate.jpeg" alt="actividades" title="actividades" />
+                </div>
+                <div>
+                    <img src="Img/fondo.jpg" alt="actividades" title="actividades" />
+                </div>
             </div>
         </div>
 
